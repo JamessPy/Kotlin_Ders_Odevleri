9b71ae9bfbdeb7b44a50ac29ab9c2ede1bf9f721
JamessPy <34818666+JamessPy@users.noreply.github.com>
2022-06-13 21:59:54 +0300

    Create Odev1.kt

---
+++ Odev1.kt
@@ -0,0 +1,27 @@
+package com.example.kotlindersleri.degiskenler
+
+fun main(){
+    val islemNo = 345
+    val ogrNo = 1202
+    val kitapNo = 28
+    val aTarih = "6/1/22"
+    val vTarih = "6/13/22"
+
+    val ogrAd = "deneme"
+    val ogrSoyAd = "test"
+    val cinsiyet = "Erkek"
+    val dTarih = "29/01/97"
+    val sinif = 4
+
+    val isBnNo = 12
+    val kitapAd = "Serenad"
+    val yazarNo = 1
+    val turNo = 2
+    val sayfaSayisi = 386
+    val puan = 9.8
+
+    val yazarAd = "Zülfü"
+    val yazarSoyad = "Livaneli"
+
+    val turAdi = "Roman"
+}
